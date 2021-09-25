fc2abb05f9f8b10a0d075aa2e82013051e6f9ffe
Raghuvansh Agarwal <rarchva@gmail.com>
2021-09-25 14:16:58 +0530

    Day 1 of 100 days of code

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,12 @@
+#1. Greeting user.
+print("Welcome To Band Name Generator\nWe make Band Naming a Piece of Cake")
+#2. Getting user's hometown.
+user_city = input("Hey, where did you grew up?\n")
+#3. Getting user's pet's name.
+user_pet_name = input("Tell me your pet's name?, I am just curious\n")
+#4. Generating cool band name.
+user_band_name = user_city + " " + user_pet_name
+#5. Showing user's their new band name
+print("Your Band Name is................... " + user_band_name)
+#6. Exiting the program
+print("Thanks for using Band Name Generator\nHope you have a nice day")
