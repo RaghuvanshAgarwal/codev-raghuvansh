cf691749b88a17d9a871b0aea334aac177bbc316
Raghuvansh Agarwal <rarchva@gmail.com>
2021-09-27 09:47:24 +0530

    Day 3 of 100

---
+++ Day 3/treasure_island.py
@@ -0,0 +1,30 @@
+print("Welcome To Treasure Island\nYour Mission is to find the treasure.")
+user_choice = ""
+user_choice = input("You Reached a Dead End,\nWhere do you want to go 'left' or 'right'\n")
+if (user_choice == "left"):
+    user_choice = input("You are at the bank of river,\nYou want to 'wait' or 'swim'\n")
+    if (user_choice == "wait"):
+        user_choice = input("You Reached at three Mystery Door,\nWhich one you want to open 'red', 'yellow', or 'blue'\n")
+        if(user_choice == "red"):
+            print("Burned by fire.\nGame Over.")
+        elif(user_choice == "yellow"):
+            print('''  
+  _________________.---.______
+ (_(______________(_o o_(____()
+              .___.'. .'.___.
+              \ o    Y    o /
+               \ \__   __/ /
+                '.__'-'__.'
+                    '''
+)
+            print("You Win\nYou found the TREASURE")
+        elif(user_choice == "blue"):
+            print("Eaten by beasts.\nGame Over.")
+        else:
+            print("Game Over")
+    else:
+        print("Attacked by trout.\nGame Over")
+else:
+    print("Fall in to a hole.\nGame Over")
+
+
