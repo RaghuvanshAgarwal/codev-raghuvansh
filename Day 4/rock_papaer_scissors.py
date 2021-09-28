b3579b2e1b2e6040d19f4bce9013a12c1b179eee
Raghuvansh Agarwal <rarchva@gmail.com>
2021-09-28 09:15:30 +0530

    Removed a IndexOutOfRange Bug in Rock Paper Scissors

--- Day 4/rock_papaer_scissors.py
+++ Day 4/rock_papaer_scissors.py
@@ -30,17 +30,20 @@ game_images = [rock,paper,scissors]
 user_hand_sign = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors "))
 computer_hand_sign = random.randint(0,2)
 
-print(f"You Chose {hand_signs[user_hand_sign]}")
-print(game_images[user_hand_sign])
-
-print(f"Computer Chose {hand_signs[computer_hand_sign]}")
-print(game_images[computer_hand_sign])
-
-
-if(user_hand_sign == 0 and computer_hand_sign == 2) or (user_hand_sign == 2 and computer_hand_sign == 1) or (user_hand_sign == 1 and computer_hand_sign == 0):
-    print("You Win")
-elif(user_hand_sign == computer_hand_sign):
-    print("Tie")
+if user_hand_sign >=3 or user_hand_sign < 0:
+    print("Invalid Input")
 else:
-    print("You Lose")
+    print(f"You Chose {hand_signs[user_hand_sign]}")
+    print(game_images[user_hand_sign])
+
+    print(f"Computer Chose {hand_signs[computer_hand_sign]}")
+    print(game_images[computer_hand_sign])
+
+
+    if(user_hand_sign == 0 and computer_hand_sign == 2) or (user_hand_sign == 2 and computer_hand_sign == 1) or (user_hand_sign == 1 and computer_hand_sign == 0):
+        print("You Win")
+    elif(user_hand_sign == computer_hand_sign):
+        print("Tie")
+    else:
+        print("You Lose")
 
