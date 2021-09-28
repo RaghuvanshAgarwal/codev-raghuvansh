396bdb258ca63fff0b43bd11f5db2846684bd036
Raghuvansh Agarwal <rarchva@gmail.com>
2021-09-28 09:12:04 +0530

    Update rock_papaer_scissors.py

--- Day 4/rock_papaer_scissors.py
+++ Day 4/rock_papaer_scissors.py
@@ -26,24 +26,15 @@ scissors = '''
 ---.__(___)
 '''
 hand_signs = ["Rock","Paper","Scissors"]
+game_images = [rock,paper,scissors]
 user_hand_sign = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors "))
 computer_hand_sign = random.randint(0,2)
 
 print(f"You Chose {hand_signs[user_hand_sign]}")
-if( user_hand_sign == 0):
-    print(rock)
-elif( user_hand_sign == 1):
-    print(paper)
-else:
-    print(scissors)
+print(game_images[user_hand_sign])
 
 print(f"Computer Chose {hand_signs[computer_hand_sign]}")
-if( computer_hand_sign == 0):
-    print(rock)
-elif( computer_hand_sign == 1):
-    print(paper)
-else:
-    print(scissors)
+print(game_images[computer_hand_sign])
 
 
 if(user_hand_sign == 0 and computer_hand_sign == 2) or (user_hand_sign == 2 and computer_hand_sign == 1) or (user_hand_sign == 1 and computer_hand_sign == 0):
